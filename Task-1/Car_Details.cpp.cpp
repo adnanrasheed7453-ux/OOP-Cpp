c4b139b1eaf5d588cbe249aea4c476e0f8ecc341
Adnan Rasheed <adnanrasheed7453@gmail.com>
2026-09-27 01:52:12 +0500

    Add files via upload

---
+++ Task-1/Car_Details.cpp.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+struct car{
+	string brand;
+	string model;
+	double price;	
+};
+int main()
+{
+	car c[3];
+	cout<<"---Enter the details---";
+	for(int i=0; i<3; i++)
+	{ 
+		cout<<"Enter the brand of car"<<i<<":";
+	cin>>c[i].brand;
+	cout<<"Enter the model of car"<<i<<":";
+	cin>>c[i].model;
+	cout<<"Enter the price of car"<<i<<":";
+	cin>>c[i].price;
+	
+	
+	
+	
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
